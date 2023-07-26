d6c06f027ad6571c3c3ded418831f25fb429ac13
Mitch Kerle <mitch@mitch>
2023-07-26 15:58:49 +1000

    Added test cases for dictHelper

--- configTemplate/util/dictHelper.py
+++ configTemplate/util/dictHelper.py
@@ -1,7 +1,7 @@
 
 def getParentXpath(xpath : str) -> str:
 
-    if (type(xpath)):
+    if (type(xpath) == str):
 
         pathParts = xpath.split('[')
 
@@ -14,6 +14,34 @@ def getParentXpath(xpath : str) -> str:
         else:
             return '['.join(pathParts[:-1])
         
+    else:
+        raise Exception('xpath is not a str')
+        
+def getChildKeyFromXpath(xpath : str) -> str:
+
+    if (type(xpath) == str):
+
+        pathParts = xpath.split('[')
+
+        pathPartsLength = len(pathParts)
+
+        if (pathPartsLength > 0):
+            childPart = pathParts[-1].replace(']', '')
+            if ('"' in childPart):
+                return childPart.replace('"', '').strip()
+            elif (childPart.isnumeric()):
+                return int(childPart)
+            else:
+                raise Exception('Unknown child type: %s' %  (str(type(childPart))))
+
+            return 
+        else:
+            raise Exception('Unable to determine child key from xpath: %s' % (xpath))
+        
+    else:
+        raise Exception('xpath is not a str')
+
+        
 def joinChildToXpath(parentXpath : str, childIndex : str | int) -> str:
 
     if (type(childIndex) == str):
@@ -22,3 +50,19 @@ def joinChildToXpath(parentXpath : str, childIndex : str | int) -> str:
         return parentXpath + '[%d]' % childIndex
     
     raise Exception('Unsupported childIndex type of %s' % (str(type(childIndex))))
+
+def getDictFromFlatternedDict(flatternedDict : dict, xpathMatch : str) -> dict:
+
+    matchedDict = {}
+    
+    if (xpathMatch in flatternedDict):
+        if (not type(flatternedDict[xpathMatch]) == dict):
+            raise Exception('xpath "%s" exists in flatternedDict and is not of type dict' % (xpathMatch))
+        else:
+            return flatternedDict[xpathMatch]
+        
+    for k in flatternedDict.keys():
+        if (xpathMatch in k):
+            matchedDict[getChildKeyFromXpath(k)] = flatternedDict[k]
+
+    return matchedDict
---
+++ tests/utilTests/testDictHelper.py
@@ -0,0 +1,115 @@
+from unittest import TestCase
+
+from configTemplate.util import dictHelper
+
+class testDictHelper(TestCase):
+
+    def testJoinXpath(self):
+
+        parent = '$["parent"]'
+        childPropertyStr = 'child'
+
+        expectedResult = parent + '["%s"]' % (childPropertyStr)
+
+        result = dictHelper.joinChildToXpath(parent, childPropertyStr)
+        self.assertTrue(result == expectedResult, 'Join xpath with child str property returned unexpected result: %s' % result)
+
+        childPropertyNum = 0
+        expectedResult = parent + '[%d]' % (childPropertyNum)
+
+        result = dictHelper.joinChildToXpath(parent, childPropertyNum)
+        self.assertTrue(result == expectedResult, 'Join xpath with child num property returned unexpected result: %s' % result)
+
+    def testGetChildKeyFromXpath(self):
+
+        xpath = '$["parent"]["foo"][0]["blah"]'
+        expectedResult = 'blah'
+
+        result = dictHelper.getChildKeyFromXpath(xpath)
+        self.assertTrue(result == expectedResult, 'Get child key returned unexpected result: %s' % (result))
+
+        xpath += '[1]' 
+        expectedResult = 1
+
+        result = dictHelper.getChildKeyFromXpath(xpath)
+        self.assertTrue(result == expectedResult, 'Get child key returned unexpected result: %s' % (str(result)))
+
+    def testGetParentFromXpath(self):
+
+        xpath = '$["parent"]["foo"][0]["blah"]'
+        expectedResult = '$["parent"]["foo"][0]'
+
+        result = dictHelper.getParentXpath(xpath)
+        self.assertTrue(result == expectedResult, 'Returned result was %s' % (result))
+
+        xpath = '$["parent"]'
+        expectedResult = '$'
+
+        result = dictHelper.getParentXpath(xpath)
+        self.assertTrue(result == expectedResult, 'Returned result was %s' % (result))
+
+        xpath = '$'
+        expectedResult = None
+
+        result = dictHelper.getParentXpath(xpath)
+        self.assertTrue(result == expectedResult, 'Returned result was %s' % (result))
+
+    def testGetDictFromFlatternedDict(self):
+
+        flatDict = {
+            '$["parent"]["foo"]' : 'bar',
+            '$["parent"]["list"][0]' : 'a',
+            '$["parent"]["list"][1]' : 'b',
+            '$["parent"]["list"][2]' : 'c',
+            '$["parent"]["blah"]["setting1"]' : 1,
+            '$["parent"]["blah"]["setting2"]' : 'x'
+        }
+
+        expectedResult = {
+            'setting1' : 1,
+            'setting2' : 'x'
+        }
+
+        result = dictHelper.getDictFromFlatternedDict(flatDict, '$["parent"]["blah"]')
+        self.assertDictEqual(result, expectedResult)
+
+        expectedResult = {
+            0 : 'a',
+            1 : 'b',
+            2 : 'c'
+        }
+
+        result = dictHelper.getDictFromFlatternedDict(flatDict, '$["parent"]["list"]')
+        self.assertDictEqual(result, expectedResult)
+
+        with self.assertRaises(Exception):
+            dictHelper.getDictFromFlatternedDict(flatDict, '$["parent"]["foo"]')
+
+        result = dictHelper.getDictFromFlatternedDict(flatDict, '$["parent"]["notexist"]')
+        self.assertDictEqual(result, {})
+
+        # expectedResult = {
+        #     'parent' : {
+        #         'foo' : 'bar',
+        #         'list' : {
+        #             0 : 'a',
+        #             1 : 'b',
+        #             2 : 'c',
+        #         },
+        #         'blah' : {
+        #             'setting1' : 1,
+        #             'setting2' : 'x'
+        #         }
+        #     }
+        # }
+        expectedResult = {            
+            'foo' : 'bar',            
+            0 : 'a',
+            1 : 'b',
+            2 : 'c',            
+            'setting1' : 1,
+            'setting2' : 'x'        
+        }
+
+        result = dictHelper.getDictFromFlatternedDict(flatDict, '$')
+        self.assertDictEqual(result, expectedResult)
